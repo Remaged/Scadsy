ef3cae2f68bb4f1b4ce160b9badea32a01b8eb94
Remaged <bob@seoduct.com>
2014-04-24 09:42:33 +0200

    Little bug fix

---
+++ application/core/SCADSY_Lang.php
@@ -0,0 +1,14 @@
+<?php (defined('BASEPATH')) OR exit('No direct script access allowed');
+
+/* load the MX_Lang class */
+require APPPATH."third_party/MX/Lang.php";
+
+/**
+ * The main language loader. This class is responsible for loading the languages.
+ */
+class SCADSY_Lang extends MX_Lang {
+}
+
+
+/* End of file SCADSY_Lang.php */
+/* Location: ./application/core/SCADSY_Lang.php */
